49d29f27e3aa5a447762e8befa6292a6ce5b1863
Erwin Olie <erwinolie@gmail.com>
2017-06-15 00:07:00 +0200

    locatie overlays

--- StenenTijdperkClient/src/presentationlayer/SpeelbordView.java
+++ StenenTijdperkClient/src/presentationlayer/SpeelbordView.java
@@ -8,30 +8,32 @@ import javafx.scene.layout.Pane;
  * SpeelbordPane.java
  * De view van het speelbord.
  * 
- * @author	Erwin Olie, s1103026
- * @version	0.1
+ * @author Erwin Olie, s1103026
+ * @version 0.2
  */
 public class SpeelbordView extends Pane {
 
-
-	/** Hier wordt het speelbord geinitialiseerd */
 	public SpeelbordView() {
-		// Het inladen van de image van het speelbord.
 		Image image = new Image("file:assets/speelbord.jpg");
 		ImageView imageView = new ImageView(image);
-		
-		imageView.setOnMouseClicked(e -> System.out.println("["+e.getX()+", "+e.getY()+"]"));
-		
-		// Voedsel : [61.0, 71.0] [241.0, 295.0]
-		// Hout : [251.0, 64.0] [389.0, 223.0]
-		// Leem : [414.0, 61.0] [570.0, 194.0]
-		// Steen : [696.0, 67.0] [830.0, 214.0]
-		// Hut : [316.0, 383.0] [397.0, 443.0]
-		// Akker : [236.0, 300.0] [354.0, 367.0]
-		// Gereedschap : [409.0, 260.0] [557.0, 389.0]
 
-		// Het toevoegen van de ingeladen elementen aan de pane.
 		this.getChildren().add(imageView);
+		new LocatieView(this, 280, 117, 100, 117); // bos
+		new LocatieView(this, 412, 113, 143, 83); // leemgroeve
+		new LocatieView(this, 714, 109, 121, 121); // steengroeve
+		new LocatieView(this, 614, 236, 127, 94); // rivier
+		new LocatieView(this, 459, 436, 94, 133); // beschavingskaart 4
+		new LocatieView(this, 553, 436, 94, 133); // beschavingskaart 3
+		new LocatieView(this, 647, 436, 94, 133); // beschavingskaart 2
+		new LocatieView(this, 741, 436, 94, 133); // beschavingskaart 1
+		new LocatieView(this, 128, 260, 110, 68); // jacht
+		new LocatieView(this, 91, 476, 82, 90); // huttegel 1
+		new LocatieView(this, 175, 476, 82, 90); // huttegel 2
+		new LocatieView(this, 259, 476, 82, 90); // huttegel 3
+		new LocatieView(this, 343, 476, 82, 90); // huttegel 4
+		new LocatieView(this, 247, 327, 75, 61); // akker
+		new LocatieView(this, 318, 399, 97, 62); // hut
+		new LocatieView(this, 418, 280, 125, 98); // gereedschapsmaker
 	}
 
 }
