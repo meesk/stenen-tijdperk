919d550b3276e967ccf6bfc62f37fdcab8f67dbc
Erwin Olie <erwinolie@gmail.com>
2017-06-16 12:33:42 +0200

    alle cirkels gedefinieerd

--- StenenTijdperkClient/src/presentationlayer/SpeelbordView.java
+++ StenenTijdperkClient/src/presentationlayer/SpeelbordView.java
@@ -32,9 +32,33 @@ public class SpeelbordView extends Pane {
 				add(new Point(40, 44));
 				add(new Point(52, 61));
 			}}), // bos
-			new LocatieView(412, 113, 143, 83, new ArrayList<>()), // leemgroeve
-			new LocatieView(714, 109, 121, 121, new ArrayList<>()), // steengroeve
-			new LocatieView(614, 236, 127, 94, new ArrayList<>()), // rivier
+			new LocatieView(412, 113, 143, 83, new ArrayList<Point>() {{
+				add(new Point(74, 4));
+				add(new Point(46, 9));
+				add(new Point(102, 10));
+				add(new Point(13, 14));
+				add(new Point(73, 25));
+				add(new Point(40, 27));
+				add(new Point(107, 28));
+			}}), // leemgroeve
+			new LocatieView(714, 109, 121, 121, new ArrayList<Point>() {{
+				add(new Point(24, 5));
+				add(new Point(54, 6));
+				add(new Point(45, 21));
+				add(new Point(74, 23));
+				add(new Point(51, 40));
+				add(new Point(78, 45));
+				add(new Point(68, 67));
+			}}), // steengroeve
+			new LocatieView(614, 236, 127, 94, new ArrayList<Point>() {{
+				add(new Point(59, 1));
+				add(new Point(30, 3));
+				add(new Point(88, 5));
+				add(new Point(53, 17));
+				add(new Point(23, 21));
+				add(new Point(80, 24));
+				add(new Point(56, 38));
+			}}), // rivier
 			new LocatieView(459, 436, 94, 133, new ArrayList<>()), // beschavingskaart 4
 			new LocatieView(553, 436, 94, 133, new ArrayList<>()), // beschavingskaart 3
 			new LocatieView(647, 436, 94, 133, new ArrayList<>()), // beschavingskaart 2
@@ -44,9 +68,16 @@ public class SpeelbordView extends Pane {
 			new LocatieView(175, 476, 82, 90, new ArrayList<>()), // huttegel 2
 			new LocatieView(259, 476, 82, 90, new ArrayList<>()), // huttegel 3
 			new LocatieView(343, 476, 82, 90, new ArrayList<>()), // huttegel 4
-			new LocatieView(247, 327, 75, 61, new ArrayList<>()), // akker
-			new LocatieView(318, 399, 97, 62, new ArrayList<>()), // hut
-			new LocatieView(418, 280, 125, 98, new ArrayList<>()) // gereedschapsmaker
+			new LocatieView(247, 327, 75, 61, new ArrayList<Point>() {{
+				add(new Point(28, 2));
+			}}), // akker
+			new LocatieView(318, 399, 97, 62, new ArrayList<Point>() {{
+				add(new Point(47, 8));
+				add(new Point(20, 13));
+			}}), // hut
+			new LocatieView(418, 280, 125, 98, new ArrayList<Point>() {{
+				add(new Point(54, 23));
+			}}) // gereedschapsmaker
 		);
 	}
 }
