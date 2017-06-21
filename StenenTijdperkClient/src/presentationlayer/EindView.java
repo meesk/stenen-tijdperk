e0b422bd0ae122f5e20503b91b1d8e8d7db67ff2
Tristan Caspers <tristancaspers97@hotmail.com>
2017-06-21 19:16:33 +0200

    Eindview merge

--- StenenTijdperkClient/src/presentationlayer/EindView.java
+++ StenenTijdperkClient/src/presentationlayer/EindView.java
@@ -16,7 +16,7 @@ import proceslayer.SpelController;
  * Een klasse die alle informatie bevat om de eind view te maken.
  *
  * @author Tristan Caspers, s1102755
- * @version 1.5
+ * @version 1.6
  */
 public class EindView extends Stage {
 
@@ -26,7 +26,7 @@ public class EindView extends Stage {
 
 		VBox vbox = new VBox(15);
 
-		// Announcement
+		// De prijsuitreiking
 		Label uitreiking = new Label();
 		uitreiking.setText("Speler ... heeft gewonnen!"); // Nog maken
 		uitreiking.setStyle("-fx-font-size: 22px");
@@ -38,6 +38,7 @@ public class EindView extends Stage {
 		yAxis.setLabel("Punten");
 		yAxis.setMinorTickVisible(false);
 
+		// Check toevoegen voor aantal spelers
 		XYChart.Series<Number, Number> data1 = new XYChart.Series<Number, Number>();
 		data1.setName("Speler 1");
 		XYChart.Series<Number, Number> data2 = new XYChart.Series<Number, Number>();
@@ -53,17 +54,14 @@ public class EindView extends Stage {
 		// for (int i = 0; i < aantalRondes.size(); i++) {} // Nog maken
 		data1.getData().add(new XYChart.Data<Number, Number>(1, 34));
 		data1.getData().add(new XYChart.Data<Number, Number>(2, 78));
-		data1.getData().add(new XYChart.Data<Number, Number>(3, 99));
 		data2.getData().add(new XYChart.Data<Number, Number>(34, 0));
 		data2.getData().add(new XYChart.Data<Number, Number>(2, 20));
-		data2.getData().add(new XYChart.Data<Number, Number>(3, 45));
-		data3.getData().add(new XYChart.Data<Number, Number>(57, 43));
 		data3.getData().add(new XYChart.Data<Number, Number>(2, 50));
 		data3.getData().add(new XYChart.Data<Number, Number>(3, 65));
-		data4.getData().add(new XYChart.Data<Number, Number>(1, 8));
 		data4.getData().add(new XYChart.Data<Number, Number>(31, 78));
 		data4.getData().add(new XYChart.Data<Number, Number>(45, 98));
 
+		// Safety type?
 		lineChart.getData().addAll(data1, data2, data3, data4);
 
 		vbox.getChildren().addAll(uitreiking, lineChart);
