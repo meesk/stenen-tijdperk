2ebe860df41b1b60a7e36367210971a04135f689
Erwin Olie <erwinolie@gmail.com>
2017-06-22 10:48:55 +0200

    4e beschavingskaart

--- StenenTijdperkServer/src/domainlayer/locaties/LocatieFactory.java
+++ StenenTijdperkServer/src/domainlayer/locaties/LocatieFactory.java
@@ -82,7 +82,7 @@ public class LocatieFactory {
 		locaties.add(new Beschavingskaart(463, 438, 85, 145, Arrays.asList(new Point(26, 18)), 0));
 		locaties.add(new Beschavingskaart(560, 438, 85, 145, Arrays.asList(new Point(26, 18)), 1));
 		locaties.add(new Beschavingskaart(653, 438, 85, 145, Arrays.asList(new Point(26, 18)), 2));
-		locaties.add(new Beschavingskaart(749, 440, 85, 145, Arrays.asList(new Point(26, 18)), 3));
+		locaties.add(new Beschavingskaart(749, 438, 85, 145, Arrays.asList(new Point(26, 18)), 3));
 	}
 	
 	private ArrayList<Point> generatePoints(int aantal){
